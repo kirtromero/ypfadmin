18d9412e71ee6966cca82d24703d46d9ee56b5d5
vagrant <forkarlmail@gmail.com>
2016-03-22 14:17:05 +0000

    added new migration files

--- database/migrations/2016_03_22_141313_create_stats_table.php
+++ database/migrations/2016_03_22_141313_create_stats_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSlugTagsTable extends Migration
+class CreateStatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,12 @@ class AddSlugTagsTable extends Migration
      */
     public function up()
     {
-        Schema::table('tags', function ($table) {
-            $table->string('slug')->after('name');
+        Schema::create('stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('affilate_id');
+            $table->integer('scene_id');
+            $table->string('user_agent');
+            $table->timestamps();
         });
     }
 
@@ -24,6 +28,6 @@ class AddSlugTagsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('stats');
     }
 }
