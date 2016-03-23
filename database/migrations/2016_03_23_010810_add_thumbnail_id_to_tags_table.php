6729b239c9c4ca3223667070dba1cd861a4176dc
vagrant <forkarlmail@gmail.com>
2016-03-23 01:09:16 +0000

    new home page

---
+++ database/migrations/2016_03_23_010810_add_thumbnail_id_to_tags_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddThumbnailIdToTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function ($table) {
+            $table->integer('thumbnail_id')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
